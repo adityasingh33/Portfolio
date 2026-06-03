ca1368e09be3f90a931866da52138021e372310f
Aditya Singh <adityasinghbharadwaj688@gmail.com>
2026-06-03 09:18:05 +0530

    refactor(app): simplify to single-page layout with ThemeProvider wrapper

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,34 +1,20 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ThemeProvider } from './context/ThemeContext'
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
-import Home from './pages/Home'
-import Project from './pages/Project'
 import Main from './pages/Main'
 
 const App = () => {
   return (
-
-    <div className='flex flex-col min-h-screen'>
-      <BrowserRouter>
+    <ThemeProvider>
+      <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
         <Navbar />
-
-        <main className='flex-1'>
-          <Routes>
-            {/* <Route path="/" element={<Home />} />
-            <Route path="/project" element={<Project />} /> */}
-
-           <Route path="/" element = {<Main/>}></Route>
-          </Routes>
+        <main style={{ flex: 1 }}>
+          <Main />
         </main>
-
         <Footer />
-      </BrowserRouter>
-    </div>
-
-
+      </div>
+    </ThemeProvider>
   );
-
-
 }
 
 export default App
